ff0b7d81c04ceff63e11bba98bb1351f8664f2ae
Adrian <riviraadrian@gmail.com>
2024-04-29 12:30:47 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "donfierrofsa.github.io/TaTeTi", // npm install --save-dev gh-pages
+  base: "https://donfierrofsa.github.io/TaTeTi/", // npm install --save-dev gh-pages     "deploy": "npm run build && gh-pages -d dist"
 });
